b38e040eb24c6eb136e068914264bb02dae08675
Yiji <offconstruction@gmail.com>
2023-09-19 18:38:15 +0800

    input data generation variations

--- data_generator/data_generator.py
+++ data_generator/data_generator.py
@@ -8,10 +8,30 @@ class DataGenerator:
         self.data = []
 
 
-    def base_generation(self, length: int, 
+    def base_generation(self, type: str, length: int, 
                         minimum: int, maximum: int) -> None:
     
-        arr = [random.randint(minimum, maximum) for _ in range(length)]
+        if type == "random":
+            arr = [random.randint(minimum, maximum) for _ in range(length)]
+
+        else:
+            if length > 1:
+                max_step = (maximum - minimum) / (length - 1)
+                step = random.uniform(0, max_step)
+            else:
+                step = 0
+
+            if type == "ascending":
+
+                arr = [int(minimum + step * i) for i in range(length)]
+                if arr[-1] > maximum:
+                    arr[-1] = maximum
+
+            elif type == "descending":
+
+                arr = [int(maximum - step * i) for i in range(length)]
+                if arr[-1] < minimum:
+                    arr[-1] = minimum
         
         append_row = {
             "length": length,
@@ -21,11 +41,13 @@ class DataGenerator:
         self.data.append(append_row)
 
 
-    def generate(self, start: int, end: int, step: int,
+    def generate(self, type: str,
+                 start: int, end: int, step: int,
                  minimum: int, maximum: int) -> pd.DataFrame:
 
         for length in range(start, end, step):
             self.base_generation(
+                type=type,
                 length=length,
                 minimum=minimum,
                 maximum=maximum
